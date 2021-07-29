b39e683df29d8e4a4af84aeae8a8d7ca74d220fa
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-07-29 16:48:28 +0200

    Add posts method and edit methods: rentals, opinions

--- app/fake_data.py
+++ app/fake_data.py
@@ -5,7 +5,7 @@ from sqlalchemy.exc import IntegrityError
 from faker import Faker
 
 from . import db
-from .models import User, Opinion, Car, Rental
+from .models import User, Opinion, Car, Rental, NewsPost
 
 
 def users(count=10):
@@ -32,9 +32,9 @@ def opinions(count=10):
     fake = Faker()
     i = 0
     images = ["opinion1.jpg", "opinion2.jpg", "opinion3.jpg"]
+    users_id = [item[0] for item in User.query.with_entities(User.id).all()]
     while i < count:
         date_str = fake.date()
-        users_id = [item[0] for item in User.query.with_entities(User.id).all()]
         opinion = Opinion(author_id=random.choice(users_id), text=fake.sentence(), image=random.choice(images),
                           date=datetime.strptime(date_str, '%Y-%m-%d').date())
         db.session.add(opinion)
@@ -65,10 +65,15 @@ def rentals(count=10):
     """Add fake rentals to db - Rental model."""
     fake = Faker()
     i = 0
+    cars_id = [item[0] for item in Car.query.with_entities(Car.id).all()]
+    users_id = [item[0] for item in User.query.with_entities(User.id).all()]
+    dates = []
+    while len(dates) < count:
+        possible_rental_from = fake.date_time()
+        if possible_rental_from not in dates:
+            dates.append(possible_rental_from)
     while i < count:
-        cars_id = [item[0] for item in Car.query.with_entities(Car.id).all()]
-        users_id = [item[0] for item in User.query.with_entities(User.id).all()]
-        rental_from = fake.date_time()
+        rental_from = dates[i]
         rental_to = fake.date_time_between_dates(datetime_start=rental_from,
                                                  datetime_end=rental_from + timedelta(days=10))
         available_date = rental_to + timedelta(hours=1)
@@ -76,6 +81,28 @@ def rentals(count=10):
                       from_date=rental_from, to_date=rental_to,
                       available_from=available_date)
         db.session.add(rent)
+        try:
+            db.session.commit()
+            i += 1
+        except IntegrityError:
+            db.session.rollback()
+
+
+def posts(count=10):
+    """Add fake posts to db - NewsPost model."""
+    fake = Faker()
+    i = 0
+    images = ["car1.jpg", "car2.jpg", "car3.jpg"]
+    users_id = [item[0] for item in User.query.with_entities(User.id).all()]
+    titles = []
+    while len(titles) < count:
+        possible_title = fake.sentence()
+        if possible_title not in titles:
+            titles.append(possible_title)
+    while i < count:
+        post = NewsPost(author_id=random.choice(users_id), title=titles[i], date=fake.date(), body=fake.text(),
+                        img_url=random.choice(images))
+        db.session.add(post)
         try:
             db.session.commit()
             i += 1
