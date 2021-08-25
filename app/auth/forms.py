90e2226c88292534a2df82d282a90111bc2918e4
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-08-25 09:38:33 +0200

    Add validation to EditMailForm, add EditPasswordForm and EditUserAdminForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 """This module stores application forms for authorization."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 from wtforms import ValidationError
-from ..models import User
+from ..models import User, Role
 
 
 class RegisterForm(FlaskForm):
@@ -56,6 +56,16 @@ class EditMailForm(FlaskForm):
     password = PasswordField("", validators=[DataRequired()])
     submit_new_mail = SubmitField("Save changes")
 
+    def validate_new_email(self, field):
+        """Custom validator - non-duplicating email.
+
+        :param field: The name of the field which will be validated.
+        :type: wtforms.fields.core.StringField
+        :raises ValidationError: Email duplicated.
+        """
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email exists.')
+
 
 class EditPasswordForm(FlaskForm):
     """Class used for edit user password.
@@ -66,3 +76,30 @@ class EditPasswordForm(FlaskForm):
                                                          message='The passwords must be identical.')])
     new_password_check = PasswordField("", validators=[DataRequired()])
     submit_new_password = SubmitField("Save changes")
+
+
+class EditUserAdminForm(FlaskForm):
+    """Class used for edit user by admin.
+    """
+    name = StringField("Name", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    email = StringField("Email: ", validators=[DataRequired(), Length(1, 80), Email()])
+    role = SelectField('Role', coerce=int)
+    telephone = StringField("Telephone", validators=[DataRequired()])
+    address = StringField("Address")
+    submit = SubmitField("Save")
+
+    def __init__(self, user, *args, **kwargs):
+        super(EditUserAdminForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        """Custom validator - non-duplicating email.
+
+        :param field: The name of the field which will be validated.
+        :type: wtforms.fields.core.StringField
+        :raises ValidationError: Email duplicated.
+        """
+        if field.data != self.user.email and User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email exists.')
