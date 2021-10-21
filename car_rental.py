a7cdf9807c69bdb78eb5006b54ff26715ccf5b2e
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-10-21 13:50:16 +0200

    Initiate tests

--- car_rental.py
+++ car_rental.py
@@ -20,5 +20,13 @@ def make_shell_context():
     return dict(db=db, User=User, Role=Role, Permission=Permission, Car=Car, NewsPost=NewsPost, Comment=Comment,
                 Rental=Rental, Opinion=Opinion)
 
+
+@app.cli.command()
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
 # if __name__ == '__main__':
 #     app.run()
