763f3aff558e35152780a6608c2aff53de95de20
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-10-25 16:23:00 +0200

    Change bad_request method - add name of variable -> validation_error

--- app/api/errors.py
+++ app/api/errors.py
@@ -4,8 +4,12 @@ from app.exceptions import ValidationError
 from . import api
 
 
-def bad_request(message):
-    response = jsonify({'error': 'bad request', 'message': message, 'success': False})
+def bad_request(message, variable=None):
+    if variable is None:
+        response = jsonify({'error': 'bad request', 'message': message, 'success': False})
+    else:
+        response = jsonify({'error_value_key': variable,
+                            'error': {'error': 'bad request', 'message': message, 'success': False}})
     response.status_code = 400
     return response
 
@@ -25,4 +29,4 @@ def forbidden(message):
 @api.errorhandler(ValidationError)
 def validation_error(e):
     """Method used for validation new data."""
-    return bad_request(e.args[0])
+    return bad_request(e.args[0], e.args[1])
