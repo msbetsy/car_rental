00678ad2015821632d76fea12f70716b40e10d9f
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-11-04 14:53:41 +0100

    Add permission required

--- app/api/users.py
+++ app/api/users.py
@@ -1,17 +1,22 @@
 """This module stores methods for users (API)."""
 from flask import jsonify
-from ..models import User
+from ..models import User, Permission
+from app.api.decorators import token_required, permission_required
 from . import api
 
 
 @api.route('/users/', methods=['GET'])
-def get_all_users():
+@token_required
+@permission_required(Permission.ADMIN)
+def get_all_users(user_id: int):
     query = User.query.all()
     users = [user.to_json() for user in query]
     return jsonify({'data': users, 'number_of_records': len(query), 'success': True})
 
 
-@api.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    query = User.query.get_or_404(id, description=f'User with id {id} not found')
+@api.route('/users/<int:user_to_show_id>', methods=['GET'])
+@token_required
+@permission_required(Permission.ADMIN)
+def get_user(user_id: int, user_to_show_id: int):
+    query = User.query.get_or_404(user_to_show_id, description=f'User with id {user_to_show_id} not found')
     return jsonify({'data': query.to_json(), 'success': True})
