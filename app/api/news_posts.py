a58ac56b9b7a17c3a37c4b7b070ee24aaa02d13c
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-11-09 18:37:40 +0100

    Add show_post, show_posts (API)

--- app/api/news_posts.py
+++ app/api/news_posts.py
@@ -4,7 +4,14 @@ from . import api
 from ..models import NewsPost
 
 
-@api.route('/posts/<int:post_id>', methods=['GET'])
+@api.route('/posts/<int:post_id>/', methods=['GET'])
 def show_post(post_id: int):
     query = NewsPost.query.get_or_404(post_id)
-    return jsonify({'success': True, 'data': ''})
+    return jsonify({'success': True, 'data': query.to_json()})
+
+
+@api.route('/posts/', methods=['GET'])
+def show_posts():
+    query = NewsPost.query.all()
+    posts = [post.to_json() for post in query]
+    return jsonify({'success': True, 'data': posts})
