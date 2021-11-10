6243f1986447f48434762dd0725be4cd207b0d2e
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-11-10 21:42:45 +0100

    Add add_post method (API)

--- app/api/news_posts.py
+++ app/api/news_posts.py
@@ -1,7 +1,9 @@
 """This module stores methods for posts (API)."""
-from flask import jsonify
+from flask import jsonify, request
 from . import api
-from ..models import NewsPost
+from ..models import NewsPost, Permission
+from app.api.decorators import validate_json_content_type, token_required, permission_required
+from app import db
 
 
 @api.route('/posts/<int:post_id>/', methods=['GET'])
@@ -15,3 +17,16 @@ def show_posts():
     query = NewsPost.query.all()
     posts = [post.to_json() for post in query]
     return jsonify({'success': True, 'data': posts})
+
+
+@api.route('/posts/', methods=['POST'])
+@token_required
+@permission_required(Permission.MODERATE)
+@validate_json_content_type
+def add_post(user_id: int):
+    args = request.get_json()
+    args['author'] = user_id
+    post = NewsPost.from_json(args)
+    db.session.add(post)
+    db.session.commit()
+    return jsonify({'success': True, 'data': post.to_json()}), 201
