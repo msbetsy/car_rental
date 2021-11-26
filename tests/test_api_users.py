f4f0b198fb0b4e5336b4f7f7937063ccb2fc3ffa
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-11-26 18:30:53 +0100

    Add test_insufficient_permissions, test_get_user_invalid_data, show_user_correct_request -> edit test_get_user

--- tests/test_api_users.py
+++ tests/test_api_users.py
@@ -2,7 +2,23 @@
 import unittest
 from app import create_app, db
 from app.models import Role, User
-from tests.api_functions import token, create_user, create_admin, create_moderator
+from tests.api_functions import token, create_user, create_admin, create_moderator, check_permissions
+
+
+def show_user_correct_request(client, user_id, api_headers):
+    """Show user data request.
+
+   :param client: Client of app
+   :type client: flask.testing.FlaskClient
+   :param user_id: Id of User
+   :type user_id: int
+   :param api_headers: Request headers
+   :type api_headers: dict
+   :return: Response for the request
+   :rtype: flask.wrappers.Response
+   """
+    response = client.get(f'/api/v1/users/{user_id}/', headers=api_headers, follow_redirects=True)
+    return response
 
 
 class UsersTestCase(unittest.TestCase):
@@ -63,13 +79,67 @@ class UsersTestCase(unittest.TestCase):
         db.session.add(user)
         db.session.commit()
 
+        del user_dict['password']
+
+        # Test request without features
+        response = show_user_correct_request(self.client, user.id, self.get_api_headers_admin())
+        response_data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+        expected_result = {'success': True,
+                           'data': {**user_dict,
+                                    'id': 4,
+                                    'post_number': 0,
+                                    'posts': [],
+                                    'rentals_number': 0,
+                                    'rentals': [],
+                                    'comments_number': 0,
+                                    'comments': [],
+                                    'opinions_number': 0,
+                                    'opinions': []
+                                    }}
+
+        self.assertDictEqual(expected_result, response_data)
+
+        # Test request with features: params
         response = self.client.get(
-            '/api/v1/users/{}'.format(user.id),
+            '/api/v1/users/{}/?params=comments,comments_number'.format(user.id),
             headers=self.get_api_headers_admin(), follow_redirects=True)
         response_data = response.get_json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
+        expected_result = {'success': True,
+                           'data': {**user_dict,
+                                    'id': 4,
+                                    'post_number': 0,
+                                    'posts': [],
+                                    'rentals_number': 0,
+                                    'rentals': [],
+                                    'opinions_number': 0,
+                                    'opinions': []
+                                    }}
+
+        self.assertDictEqual(expected_result, response_data)
+
+    def test_get_user_invalid_data(self):
+        """Test api for get_user, correct data."""
+        user_dict = {
+            'email': 'user1@example.com', 'name': 'user1_name', 'surname': 'user1_surname', 'telephone': 123,
+            'password': 'password', 'role_id': 1, 'address': None
+        }
+        user = User(**user_dict)
+        db.session.add(user)
+        db.session.commit()
+
         del user_dict['password']
+
+        # Test request with features: params
+        response = self.client.get(
+            '/api/v1/users/{}/?params=commentscomments_number'.format(user.id),
+            headers=self.get_api_headers_admin(), follow_redirects=True)
+        response_data = response.get_json()
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
         expected_result = {'success': True,
                            'data': {**user_dict,
                                     'id': 4,
@@ -119,3 +189,20 @@ class UsersTestCase(unittest.TestCase):
                            ]
                            }
         self.assertDictEqual(expected_result, response_data)
+
+    # Test permissions
+    def test_insufficient_permissions(self):
+        """Test permissions."""
+        # Check permissions for get_user
+        user_id = User.query.filter_by(email='test@test.com').first().id
+        api_headers = self.get_api_headers_admin()
+
+        # Test user permissions
+        api_headers['Authorization'] = f'Bearer {self.token}'
+        response = show_user_correct_request(self.client, user_id, api_headers)
+        check_permissions(response, self.assertEqual, self.assertFalse)
+
+        # Test moderator permissions
+        api_headers['Authorization'] = f'Bearer {self.token_moderator}'
+        response = show_user_correct_request(self.client, user_id, api_headers)
+        check_permissions(response, self.assertEqual, self.assertFalse)
