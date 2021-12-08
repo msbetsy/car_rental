583cb50f8ebb400ff35852dbe3a25a08fb2a58a3
msbetsy <78367584+msbetsy@users.noreply.github.com>
2021-12-08 15:12:03 +0100

    Delete replybutton function

--- app/static/js/js_script.js
+++ app/static/js/js_script.js
@@ -1,8 +1,3 @@
-function replybutton(btn) {
-  btn.nextElementSibling.style.display = "block";
-  btn.style.display = "none"
-  }
-
 var formErrors = {% if form.errors %}true{% else %}false{% endif %};
 
 $(document).ready(function() {
